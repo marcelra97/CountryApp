e510bc9cde4443d812df651561359d4a5014543a
Marcel Ramirez <marcelramireznovillo@gmail.com>
2024-02-24 10:44:23 +0100

    Mejorando y puliendo la aplicacion de los paises

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,6 @@
-import { Country } from '../../interfaces/country';
+import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from './../../services/countries.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,16 +8,27 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ByCapitalPageComponent {
+export class ByCapitalPageComponent implements OnInit{
 
   public countries: Country[] = []
+  public isLoading: boolean = false;
+  public initialValue: string = '';
 
   constructor( private countriesService: CountriesService) {}
 
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCapital.countries;
+    this.initialValue = this.countriesService.cacheStore.byCapital.term;
+  }
+
   searchByCapital( term: string ) {
+
+    this.isLoading = true;
+
     this.countriesService.searchCapital( term )
       .subscribe( countries => {
         this.countries = countries;
+        this.isLoading = false;
       });
 
   }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,16 +5,18 @@ import { RouterModule } from '@angular/router';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
     ContactPageComponent,
     HomePageComponent,
-    SidebarComponent,
+    LoadingSpinnerComponent,
     SearchBoxComponent,
+    SidebarComponent,
   ],
   imports: [
     CommonModule,
@@ -24,8 +26,9 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     AboutPageComponent,
     ContactPageComponent,
     HomePageComponent,
-    SidebarComponent,
+    LoadingSpinnerComponent,
     SearchBoxComponent,
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
